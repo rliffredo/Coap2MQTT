77c6b48723435d75e59bea51f111d1772ae3159a
Roberto Liffredo <roberto@liffredo.net>
2025-12-31 18:30:46 +0100

    Improved MQTT handling - Better logging - Fixed crash on disconnection

--- publisher.py
+++ publisher.py
@@ -1,39 +1,76 @@
+import asyncio
 import datetime
 import json
 import logging
 from contextlib import asynccontextmanager
 
 import aiomqtt
+from aiomqtt import MqttCodeError
 
 from philips_hu1508 import parse_state
 
 logger = logging.getLogger(__name__)
 
 class MQTTPublisher:
-	def __init__(self, client: aiomqtt.Client, root: str):
-		self.client = client
+	def __init__(self, server: str, port: int, root: str):
+		self.reconnecting = False
+		self.connected = False
+		self.client = aiomqtt.Client(server, port)
+		self.server = server
+		self.port = port
 		self.root = root
 		self.last_states = {}
+		self.connection_lock = asyncio.Lock()
+
+	async def _connect(self):
+		async with self.connection_lock:
+			if self.connected:
+				return
+			logger.info("Connecting to %s...", self.server)
+			await self.client.__aenter__()
+			self.connected = True
+
+	async def _disconnect(self):
+		async with self.connection_lock:
+			if not self.connected:
+				return
+			logger.info("Disconnecting from %s...", self.server)
+			try:
+				await self.client.__aexit__(None, None, None)
+			except MqttCodeError as e:
+				logger.warning("Could not disconnect, marking it as disconnected anyways. Error: %s", e)
+			self.connected = False
+	
+	async def _publish(self, host: str, key: str, payload: int | float | str):
+		await self._connect()
+		try:
+			await self.client.publish(f"{self.root}/{host}/{key}", payload=payload)
+		except MqttCodeError as e:
+			logger.error("Could not publish data: [%s]", e)
+			await self._disconnect()
 
 	async def publish_state(self, host, state) -> None:
-		await self.client.publish(f"{self.root}/{host}/last_update", payload=datetime.datetime.now().isoformat())
-		await self.client.publish(f"{self.root}/{host}/raw_state", payload=json.dumps(state))
+		raw_state = json.dumps(state)
+		logger.debug("Publishing state for %s: %s", host, raw_state)
+		await self._publish(host, "last_update", datetime.datetime.now().isoformat())
+		await self._publish(host, "raw_state", raw_state)
 		pretty_state = parse_state(state)
 		last_state = self.last_states.get(host, {})
 		for key, value in pretty_state.items():
 			if value == last_state.get(key, None):
 				continue
-			await self.client.publish(f"{self.root}/{host}/{key}", payload=value)
+			await self._publish(host, key, value)
 		self.last_states[host] = pretty_state
 
 	async def publish_online(self, host) -> None:
-		await self.client.publish(f"{self.root}/{host}/status", payload="ONLINE")
+		await self._publish(host, "status", "ONLINE")
 
 	async def publish_offline(self, host) -> None:
-		await self.client.publish(f"{self.root}/{host}/status", payload="OFFLINE")
+		await self._publish(host, "status", "OFFLINE")
 
 	@staticmethod
 	@asynccontextmanager
 	async def create(server: str, root: str, port: int = 1883):
-		async with aiomqtt.Client(server, port) as client:
-			yield MQTTPublisher(client, root)
+		publisher = MQTTPublisher(server, port, root)
+		yield publisher
+		await publisher._disconnect()
